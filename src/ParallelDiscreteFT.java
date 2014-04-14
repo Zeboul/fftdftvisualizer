ca3cb26dfbc784ba01ec916e37f67d456cd78dbe
Patrick Gauvin <pggauvin@gmail.com>
2014-04-14 14:37:27 -0400

    Fixed ParalellDiscreteFT.java.  Now should see large performance increase with parallel algorithm now.

--- src/ParallelDiscreteFT.java
+++ src/ParallelDiscreteFT.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.*;
 
 
 public class ParallelDiscreteFT extends SequentialDiscreteFT
@@ -17,27 +18,29 @@ public class ParallelDiscreteFT extends SequentialDiscreteFT
 		int NUM_PROCS = Runtime.getRuntime().availableProcessors();
 		Thread[] thread = new Thread[NUM_PROCS];
 		SequentialDiscreteFT[] sequentialDiscreteFT = new SequentialDiscreteFT[NUM_PROCS];
-		List<Queue<double[]>> threadQueues = new LinkedList<Queue<double[]>>();
+		List<ConcurrentLinkedQueue<double[]>> threadQueues = new LinkedList<ConcurrentLinkedQueue<double[]>>();
 		double[] data = null;
 		
 		try
 		{
 			for (int i = 0, s = startPos; i < NUM_PROCS; i++, s += incSize)
 			{
-				threadQueues.add(new LinkedList<double[]>());
+				threadQueues.add(new ConcurrentLinkedQueue<double[]>());
 				sequentialDiscreteFT[i] = new SequentialDiscreteFT(threadQueues.get(i), song, numBins, s,
 						incSize * NUM_PROCS);
 				thread[i] = new Thread(sequentialDiscreteFT[i]);
-				thread[i].start();
 			}
 
-			for (int i = 0; i + numBins < datas.length; i += incSize)
+			for (int i = 0; i < NUM_PROCS; i++)
+				thread[i].start();
+
+			for (int i = 0; i < datas.length / incSize - 1; i++)
 			{
 				data = null;
 
 				do
 				{
-					data = threadQueues.get((i / incSize) % NUM_PROCS).poll();
+					data = threadQueues.get(i % NUM_PROCS).poll();
 				} while (data == null);
 
 				while (!queue.offer(data));
